977d5e45428c1f200683f6eb3de19d09dfa19d43
TongKuai <tiantian@tongkuai.net>
2013-03-26 18:48:42 +0800

    增加全局错误处理

---
+++ TongKuai.Web/Controllers/ErrorController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TongKuai.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        //
+        // GET: /Error/
+
+        public ActionResult Index(string error)
+        {
+            ViewBag.Title = "WebSite 网站内部错误";
+            ViewBag.Description = error;
+            return View("Index");
+        }
+
+        public ActionResult HttpError404(string error)
+        {
+            ViewBag.Title = "HTTP 404- 无法找到文件";
+            ViewBag.Description = error;
+            return View("Index");
+        }
+
+        public ActionResult HttpError500(string error)
+        {
+            ViewBag.Title = "HTTP 500 - 内部服务器错误";
+            ViewBag.Description = error;
+            return View("Index");
+        }
+
+        public ActionResult General(string error)
+        {
+            ViewBag.Title = "HTTP 发生错误";
+            ViewBag.Description = error;
+            return View("Index");
+        }
+
+    }
+}
--- TongKuai.Web/Global.asax.cs
+++ TongKuai.Web/Global.asax.cs
@@ -30,5 +30,43 @@ namespace TongKuai.Web
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
         }
+
+        /// <summary>
+        /// 全局错误处理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+            Response.Clear();
+            HttpException httpException = exception as HttpException;
+            RouteData routeData = new RouteData();
+            routeData.Values.Add("controller", "Error");
+            if (httpException == null)
+            {
+                routeData.Values.Add("action", "Index");
+            }
+            else
+            {
+                switch (httpException.GetHttpCode())
+                {
+                    case 404:
+                        routeData.Values.Add("action", "HttpError404");
+                        break;
+                    case 500:
+                        routeData.Values.Add("action", "HttpError500");
+                        break;
+                    default:
+                        routeData.Values.Add("action", "General");
+                        break;
+                }
+            }
+            routeData.Values.Add("error", exception.Message);
+            Server.ClearError();
+            IController errorController = new Controllers.ErrorController();
+            errorController.Execute(new RequestContext(
+           new HttpContextWrapper(Context), routeData));
+        }
     }
 }
